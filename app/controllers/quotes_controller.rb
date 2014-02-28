f8d2e6b6ac667d8aecc6b19576c906f4023b757e
Kevin Pan <xcpankevin@hotmail.com>
2014-02-27 23:06:35 -0500

    Adds stream to database if stream is new. Changes ordering of how quotes are displayed to be ordered by date from latest to oldest

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,6 +1,6 @@
 class QuotesController < ApplicationController
 	def index
-		@quotes = Quote.all
+		@quotes = Quote.order(created_at: :desc)
 	end
 
 	def new
@@ -9,15 +9,36 @@ class QuotesController < ApplicationController
 
 	def create
 		@quote = Quote.new(quote_params)
-		if @quote.save
-			redirect_to quotes_path
+		stream = Stream.new(stream_params)
+
+		if Stream.exists?(stream_id: params[:quote][:stream_id])
+			save_quote(@quote)
 		else
-			render 'new'
+			if stream.save
+				save_quote(@quote)	
+			else
+				flash[:error] = "Failed to save stream"
+				render 'new'
+			end
 		end
 	end
 
 	private
 		def quote_params
 			params.require(:quote).permit(:quote, :stream_id)
+		end
+
+		def stream_params
+			params.permit(:stream_id, :stream_name, :stream_url)
+			return_params = ActionController::Parameters.new(stream_id: params[:quote][:stream_id], name: params[:stream_name], url: params[:stream_url] )
+			return_params.permit!
+		end
+
+		def save_quote(quote)
+			if quote.save
+				redirect_to quotes_path
+			else
+				render 'new'
+			end
 		end
 end

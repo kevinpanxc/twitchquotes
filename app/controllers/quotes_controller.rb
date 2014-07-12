3acbd1d02ae49475a8e71f928c222e04ab12fc12
Kevin Pan <xcpankevin@hotmail.com>
2014-07-12 01:44:22 -0400

    Adds new edit/update quote actions intended for site admins

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,63 +1,78 @@
-class QuotesController < ApplicationController	
-	def index
-		@quote_dom_id = 0
-		@quotes = Quote.paginate(page: params[:page], :per_page => 10, order: "created_at DESC")
-	end
+class QuotesController < ApplicationController
+    before_filter :signed_in_user, only: [:edit, :update]
 
-	def new
-		@quote = Quote.new
-	end
+    def index
+        @quote_dom_id = 0
+        @quotes = Quote.paginate(page: params[:page], :per_page => 10, order: "created_at DESC")
+    end
 
-	def show
-		@quote_dom_id = 0
-		@quote = Quote.find(params[:id])
-	end
+    def new
+        @quote = Quote.new
+    end
 
-	def create
-		@quote = Quote.new(quote_params)
-		@stream = Stream.new(stream_params)
+    def show
+        @quote_dom_id = 0
+        @quote = Quote.find(params[:id])
+    end
 
-		if Stream.exists?(stream_id: params[:quote][:stream_id])
-			save_quote(@quote)
-		else
-			if @stream.save
-				save_quote(@quote)
-			else
-				flash.now[:error] = "Failed to save stream"
-				render 'new'
-			end
-		end
-	end
+    def edit
+        @quote = Quote.find(params[:id])
+    end
 
-	def destroy
-		@quote = Quote.find(params[:id])
-		@quote.stream.quotes_count = @quote.stream.quotes.count
-		@quote.delete
-		redirect_to quotes_path
-	end
+    def update
+        @quote = Quote.find(params[:id])
+        if @quote.update_attributes(quote_params)
+            redirect_to @quote
+        else
+            render 'edit'
+        end
+    end
 
-	private
-		def quote_params
-			params.require(:quote).permit(:quote, :stream_id, :title)
-		end
+    def create
+        @quote = Quote.new(quote_params)
+        @stream = Stream.new(stream_params)
 
-		def stream_params
-			params.permit(:stream_id, :stream_name, :stream_url)
-			return_params = ActionController::Parameters.new(stream_id: params[:quote][:stream_id], 
-				name: params[:stream_name], 
-				url: params[:stream_url],
-				logo: params[:stream_logo],
-				followers: params[:stream_followers] )
-			return_params.permit!
-		end
+        if Stream.exists?(stream_id: params[:quote][:stream_id])
+            save_quote(@quote)
+        else
+            if @stream.save
+                save_quote(@quote)
+            else
+                flash.now[:error] = "Failed to save stream"
+                render 'new'
+            end
+        end
+    end
 
-		def save_quote(quote)
-			if quote.save
-				quote.stream.quotes_count = quote.stream.quotes.count
-				quote.stream.save
-				redirect_to quotes_path
-			else
-				render 'new'
-			end
-		end
+    def destroy
+        @quote = Quote.find(params[:id])
+        @quote.stream.quotes_count = @quote.stream.quotes.count
+        @quote.delete
+        redirect_to quotes_path
+    end
+
+    private
+        def quote_params
+            params.require(:quote).permit(:quote, :stream_id, :title)
+        end
+
+        def stream_params
+            params.permit(:stream_id, :stream_name, :stream_url)
+            return_params = ActionController::Parameters.new(stream_id: params[:quote][:stream_id], 
+                name: params[:stream_name], 
+                url: params[:stream_url],
+                logo: params[:stream_logo],
+                followers: params[:stream_followers] )
+            return_params.permit!
+        end
+
+        def save_quote(quote)
+            if quote.save
+                quote.stream.quotes_count = quote.stream.quotes.count
+                quote.stream.save
+                redirect_to quotes_path
+            else
+                render 'new'
+            end
+        end
 end

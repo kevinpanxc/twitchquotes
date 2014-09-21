3544ba5bb6b2c0538c12c960b5b97f94d3dedc01
Kevin Pan <xcpankevin@hotmail.com>
2014-09-20 19:42:44 -0700

    Adds exception handling in the stream updater module to report streams that failed to update

--- lib/stream_updater.rb
+++ lib/stream_updater.rb
@@ -3,11 +3,15 @@ module StreamUpdater
         streams = Stream.all
 
         streams.each do |stream|
-            json = TwitchApi.get_twitch_channel_by_name(CGI::escape stream.name)
-            stream.followers = json["followers"]
-            stream.logo = json["logo"]
-            stream.quotes_count = stream.quotes.count
-            stream.save
+            begin
+                json = TwitchApi.get_twitch_channel_by_name(CGI::escape stream.name)
+                stream.followers = json["followers"]
+                stream.logo = json["logo"]
+                stream.quotes_count = stream.quotes.count
+                stream.save
+            rescue
+                puts "Stream #{stream.name} did not successfully update"
+            end
         end
     end
 end

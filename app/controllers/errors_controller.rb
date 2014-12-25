a2dbcd21707b95c780b95dd546c6da50e6ca4f38
Kevin Pan <xcpankevin@hotmail.com>
2014-12-24 19:31:03 -0800

    Minor change to the errors_controller

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,16 +1,16 @@
 class ErrorsController < ApplicationController
     def error_404
         @status = 404
-        render 'error_page', status: 404
+        render 'error_page', status: @status
     end
 
     def error_422
         @status = 422
-        render 'error_page', status: 422
+        render 'error_page', status: @status
     end
 
     def error_500
         @status = 500
-        render 'error_page', status: 500
+        render 'error_page', status: @status
     end
 end

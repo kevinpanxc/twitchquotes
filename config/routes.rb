97452891c00a9da6a298f51fe2120f8588337c23
Kevin Pan <xcpankevin@hotmail.com>
2014-12-24 18:33:05 -0800

    Changes the match method to get in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,7 @@ Twitchquotes::Application.routes.draw do
   get 'facebook/signout', to: 'facebook_sessions#destroy', as: 'facebook_signout'
 
   # error routing
-  match '/404', to: 'errors#error_404'
-  match '/422', to: 'errors#error_422'
-  match '/500', to: 'errors#error_500'
+  get '/404', to: 'errors#error_404'
+  get '/422', to: 'errors#error_422'
+  get '/500', to: 'errors#error_500'
 end

efa44a7e8f1104416e151739cadac2def11bb05e
Kevin Pan <xcpankevin@hotmail.com>
2014-12-24 19:28:59 -0800

    Error pages render with the proper status code

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,16 +1,16 @@
 class ErrorsController < ApplicationController
     def error_404
         @status = 404
-        render 'error_page'
+        render 'error_page', status: 404
     end
 
     def error_422
         @status = 422
-        render 'error_page'
+        render 'error_page', status: 422
     end
 
     def error_500
         @status = 500
-        render 'error_page'
+        render 'error_page', status: 500
     end
 end
